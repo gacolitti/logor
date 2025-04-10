7578769361d895cbe90228912bcb42c35e301885
Giovanni Colitti <g.a.colitti@gmail.com>
2025-04-10 10:24:46 -0500

    remove unused files

--- tests/testthat/test-logor.R
+++
@@ -1,9 +0,0 @@
-test_that("get_company_logo works", {
-  skip("Not implemented yet")
-  # TODO: Add tests for company logo fetching
-})
-
-test_that("get_crypto_logo works", {
-  skip("Not implemented yet")
-  # TODO: Add tests for crypto logo fetching
-})
